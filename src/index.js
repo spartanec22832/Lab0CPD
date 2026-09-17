9f8cfff36b42711c3f3f7b6176e0b5454a2b476f
spartanec22832 <b.bogovski@yandex.ru>
2026-09-17 16:07:37 +0300

    remove deprecated symbs

--- src/index.js
+++ src/index.js
@@ -15,22 +15,22 @@ function trapezoidalIntegrate(func, a, b, n) {
     return sum * h
 }
 
-const inputA = prompt("Введите начальную точку интегрирования a:", "0");
-const inputB = prompt("Введите конечную точку b:", "10");
-const inputN = prompt("Введите количество отрезков разбиения n:", "1000");
+const inputA = prompt("Введите начальную точку интегрирования a:", "0")
+const inputB = prompt("Введите конечную точку b:", "10")
+const inputN = prompt("Введите количество отрезков разбиения n:", "1000")
 
-const a = parseFloat(inputA);
-const b = parseFloat(inputB);
-const n = parseInt(inputN);
+const a = parseFloat(inputA)
+const b = parseFloat(inputB)
+const n = parseInt(inputN)
 
 
 if (!isNaN(a) && !isNaN(b) && !isNaN(n) && n > 0) {
 
-    const result = trapezoidalIntegrate(func, a, b, n);
+    const result = trapezoidalIntegrate(func, a, b, n)
 
-    console.log(`Результат интегрирования методом трапеций от ${a} до ${b} при n = ${n} равен: ${result}`);
-    alert(`Результат вычисления: ${result.toFixed(4)}`);
+    console.log(`Результат интегрирования методом трапеций от ${a} до ${b} при n = ${n} равен: ${result}`)
+    alert(`Результат вычисления: ${result.toFixed(4)}`)
 
 } else {
-    alert("Неверный ввод! Учтите, что n > 0");
+    alert("Неверный ввод! Учтите, что n > 0")
 }
